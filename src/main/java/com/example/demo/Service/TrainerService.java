eb613ea0f2a548cd6683bfa021e133b93d5af031
Shen Liao <shen.liao@thoughtworks.com>
2020-09-24 20:52:52 +0800

    [Nelson Liao] feat: finish get /trainer API

--- src/main/java/com/example/demo/Service/TrainerService.java
+++ src/main/java/com/example/demo/Service/TrainerService.java
@@ -2,9 +2,14 @@ package com.example.demo.Service;
 
 import com.example.demo.common.Converters;
 import com.example.demo.domain.Trainer;
+import com.example.demo.entity.TraineeEntity;
 import com.example.demo.entity.TrainerEntity;
 import com.example.demo.repository.TrainerRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @Service
 public class TrainerService {
@@ -17,5 +22,17 @@ public class TrainerService {
     public TrainerEntity addTrainee(Trainer trainer) {
         TrainerEntity trainerSaving = Converters.trainerEntityConverter(trainer);
         return this.trainerRepository.save(trainerSaving);
+    }
+
+    public List<TrainerEntity> findAllTrainers() {
+        Iterable<TrainerEntity> trainerEntities = this.trainerRepository.findAll();
+        List<TrainerEntity> trainerEntityList = new ArrayList<>();
+
+        Iterator<TrainerEntity> trainerEntitiesIterator = trainerEntities.iterator();
+        while(trainerEntitiesIterator.hasNext()) {
+            trainerEntityList.add(trainerEntitiesIterator.next());
+        }
+
+        return trainerEntityList;
     }
 }
--- src/main/java/com/example/demo/api/TrainerController.java
+++ src/main/java/com/example/demo/api/TrainerController.java
@@ -3,11 +3,13 @@ package com.example.demo.api;
 import com.example.demo.Service.TrainerService;
 import com.example.demo.domain.Trainee;
 import com.example.demo.domain.Trainer;
+import com.example.demo.entity.TraineeEntity;
 import com.example.demo.entity.TrainerEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/trainers")
@@ -22,5 +24,10 @@ public class TrainerController {
     @ResponseStatus(HttpStatus.CREATED)
     public TrainerEntity addTrainee(@RequestBody @Valid Trainer trainer) {
         return this.trainerService.addTrainee(trainer);
+    }
+
+    @GetMapping
+    public List<TrainerEntity> getTrainees() {
+        return this.trainerService.findAllTrainers();
     }
 }
